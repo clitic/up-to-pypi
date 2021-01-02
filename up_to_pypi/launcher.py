9dfe7952717b3c184cd2778b416aa992719b9133
360modder <71246790+360modder@users.noreply.github.com>
2021-01-02 13:56:03 +0530

    Add files via upload

--- up_to_pypi/launcher.py
+++ up_to_pypi/launcher.py
@@ -3,20 +3,7 @@ import os
 abs_path = os.path.dirname(__file__)
 
 def main():
-	try:
-		import PyQt5
-		import qtmodern.styles
-		import qtmodern.windows
-	except:
-		# Slow Loading
-		os.startfile(f"{abs_path}\\up_to_pypi.exe")
-		print("Loading EXE . . .")
-		exit()
-
-	# Faster Loading
 	os.startfile(f"{abs_path}\\main.pyw")
-	print("Loading PYW . . .")
-	exit()
 
-if __name__ == '__main__':
-	main()
+def awc():
+	os.startfile(f"{abs_path}\\wheel_window.pyw")
